7728d51af1726d5b199132dcd3916a947a9fbf2c
Juan dD Aceituno <juandediosaceituno@gmail.com>
2026-06-26 14:45:06 -0400

    Fix: add appcompat and activity dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
