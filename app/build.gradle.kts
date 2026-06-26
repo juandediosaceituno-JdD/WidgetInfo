9c53a53e16cde16aa59b40f8fea4b56d53511750
Juan dD Aceituno <juandediosaceituno@gmail.com>
2026-06-26 15:05:01 -0400

    Fix widget: update for Android 16, compileSdk 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.jdd.weatherwidget"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.jdd.weatherwidget"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
